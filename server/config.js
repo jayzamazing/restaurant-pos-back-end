8139dbe99ac1e4e89eb28ee7b318b58a1642234e
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-07-26 11:35:58 -0400

    Added enviromental variables to config.js for user password for database.

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@
     exports.DATABASE_URL = process.env.DATABASE_URL ||
         global.DATABASE_URL ||
         (process.env.NODE_ENV === 'production' ?
-        'mongodb://<dbuser>:<dbpassword>@ds029735.mlab.com:29735/restaurant-pos-db' :
+        'mongodb://' + process.env.dbuser +':' + process.env.dbpassword +'@ds029735.mlab.com:29735/restaurant-pos-db' :
         'mongodb://localhost/restaurant-pos-dev');
     exports.PORT = process.env.PORT || 8000;
 })();
