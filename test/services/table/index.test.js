a6cc2cf166c8bfbc5cf1c13496ee7abb46f1ef9d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-17 08:41:42 -0400

    Added get test for test/services/table/index.text.js

--- test/services/table/index.test.js
+++ test/services/table/index.test.js
@@ -31,7 +31,57 @@ describe('table service', function() {
     //once listening do the following
     this.server.once('listening', () => {
       //go through mongoose and create some mock items for testing
-      //TODO create mock tables
+      Table.create({
+        tableId: 1,
+        checkNumber: 1,
+        order: [{
+          dish: 'omlette',
+          notes: 'cheddar, bacon only',
+          cost: 4.99
+        },
+        {
+          dish: 'coffee',
+          notes: '',
+          cost: 1.99
+        }]
+      });
+      Table.create({
+        tableId: 2,
+        checkNumber: 1,
+        order: [{
+          dish: 'reuben',
+          notes: 'provolone, side of coleslaw',
+          cost: 7.99
+        },
+        {
+          dish: 'soda',
+          notes: '',
+          cost: 1.99
+        }]
+      });
+      Table.create({
+        tableId: 3,
+        checkNumber: 1,
+        order: [{
+          dish: 'chicken sandwhich',
+          notes: 'munster, easy on mayo',
+          cost: 7.99
+        },
+        {
+          dish: 'tea',
+          notes: '',
+          cost: 1.99
+        }]
+      });
+      Table.create({
+        tableId: 3,
+        checkNumber: 2,
+        order: [{
+          dish: 'fish n chips',
+          notes: 'to-go',
+          cost: 8.99
+        }]
+      });
       //create mock user
       User.create({
         'username': 'resposadmin',
@@ -73,6 +123,38 @@ describe('table service', function() {
   it('registered the tables service', () => {
     assert.ok(app.service('tables'));
   });
+  //test for get /table
+  it('should get the table data', function(done) {
+    //setup a request
+    chai.request(app)
+      //request to /
+      .get('/tables')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'Bearer '.concat(token))
+      //when finished do the following
+      .end(function(err, res) {
+        //check server gives 201 response and the data sent back from the server
+        res.should.have.status(200);
+        res.body.data[0].tableId.should.be.a('number');
+        res.body.data[0].tableId.should.equal(1);
+        res.body.data[0].checkNumber.should.be.a('number');
+        res.body.data[0].checkNumber.should.equal(1);
+        res.body.data[0].order.should.be.a('array');
+        res.body.data[0].order[0].dish.should.be.a('string');
+        res.body.data[0].order[0].dish.should.equal('omlette');
+        res.body.data[0].order[0].notes.should.be.a('string');
+        res.body.data[0].order[0].notes.should.equal('cheddar, bacon only');
+        res.body.data[0].order[0].cost.should.be.a('number');
+        res.body.data[0].order[0].cost.should.equal(4.99);
+        res.body.data[0].order[1].dish.should.be.a('string');
+        res.body.data[0].order[1].dish.should.equal('coffee');
+        res.body.data[0].order[1].notes.should.be.a('string');
+        res.body.data[0].order[1].notes.should.equal('');
+        res.body.data[0].order[1].cost.should.be.a('number');
+        res.body.data[0].order[1].cost.should.equal(1.99);
+        done();
+      });
+    });
   //test for post for /guest to add order data to a table and specific check
   it('should post the guest order data', function(done) {
     //setup a request
@@ -88,7 +170,7 @@ describe('table service', function() {
         order: [{
           dish: 'cheeseburger',
           notes: 'swiss cheese',
-          cost: '8.99'
+          cost: 8.99
         }]
       })
       //when done do the following
@@ -96,6 +178,17 @@ describe('table service', function() {
         console.log(res.body);
         //check server gives 201 response
         res.should.have.status(201);
+        res.body.tableId.should.be.a('number');
+        res.body.tableId.should.equal(3);
+        res.body.checkNumber.should.be.a('number');
+        res.body.checkNumber.should.equal(2);
+        res.body.order.should.be.a('array');
+        res.body.order[0].dish.should.be.a('string');
+        res.body.order[0].dish.should.equal('cheeseburger');
+        res.body.order[0].notes.should.be.a('string');
+        res.body.order[0].notes.should.equal('swiss cheese');
+        res.body.order[0].cost.should.be.a('number');
+        res.body.order[0].cost.should.equal(8.99);
         done();
       });
 
