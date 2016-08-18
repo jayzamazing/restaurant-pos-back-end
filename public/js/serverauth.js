76b40be55ff19dcc5e45f6162f57bcab28bbad9d
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-18 13:18:44 -0400

    Trying out feathers client side authentication in public/js/serverauth.js, added config for feathers and app.authenticate to clientauth. Added storetoken function to public/js/storeAuth.js. Added name to form in login.html.

--- public/js/serverauth.js
+++ public/js/serverauth.js
@@ -1,3 +1,18 @@
 'use strict';
 var serverAuth = angular.module('serverAuth', []);
-serverAuth.factory('clientAuth', )
+//set up feathers client side
+let app = feathers()
+.configure(hooks())
+.configure(authentication({
+  storage: window.localStorage
+}))
+
+serverAuth.factory('clientAuth',
+function() {
+  return app.authenticate({
+    type: 'local'
+  }, {}, {
+    auth: {method: 'POST, cache: false, isArray: false'}
+  });
+});
+serverAuth.factory('storeToken', );
--- public/js/storeAuth.js
+++ public/js/storeAuth.js
@@ -1,5 +1,28 @@
 'use strict';
 
 var storeAuth = angular.module('storeAuth', []);
-storeAuth.controller('AuthUser',
-['$scope', '$location', 'clientAuth', 'storeToken'])
+storeAuth.controller('AuthUser', ['$scope', '$location', 'clientAuth', 'storeToken',
+function (sc, loc, clientAuth, storeToken) {
+  //function to call login in serverauth
+  sc.login = function() {
+    sc.sub = true;
+    var postData = {
+      'username' : sc.username,
+      'storenumber' : sc.storeNumber,
+      'password' : sc.password
+    };
+    clientAuth.auth({}, postData,
+    function success(res) {
+      if (res.userAuth === 'auth') {
+        storeToken(res.token, new Date());
+        loc.path('');//TODO decide on path
+      } else {
+        sc.error = 'Login Failed';
+      }
+      postData = null;
+    },
+  function error(err) {
+    console.error('Error: ' + JSON.stringify(err));
+  });
+  };
+}]);
