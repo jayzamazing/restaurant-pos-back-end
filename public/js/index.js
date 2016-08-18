029acb311aadbd1c9340fb9a1e9e27634b57551a
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-17 21:24:12 -0400

    Added ngroute and added / and /index routes to public/js/index.js.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
-/*jshint esversion: 6 */
-var $ = require('jquery');
-$('document').ready(function(event) {
-    'use strict';
+'use strict';
+//set ngroute as dependency of app
+var viewModel = angular.module('viewModel', ['ngRoute']);
+//use routeprovider to create different routes in the application
+viewModel.config(['$routeProvider', '$locationProvider',
+  viewModel.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      //when / is requested, set login.html
+      .when('/', {
+        templateUrl: 'login.html'
+      }).otherwise({
+        redirectTo: '/index'
+      });
+    //change browsers url
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  })
+]);
+viewModel.run(function (sendHead) {
+  sendHead.init();
 });
