f680579817087374665063c26aab4fda0fd964ef
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-24 21:29:57 -0400

    Added get attribute and apply to menuitems function in public/js/storeorders.js. Added ng-click in public/templates/orders.html.

--- public/js/storeorders.js
+++ public/js/storeorders.js
@@ -5,7 +5,6 @@ storeOrders.controller('OrderData', ['$scope', '$location', 'DataStore', 'Catego
 function ($scope, $location, DataStore, Categories, Menus) {
   //variable that tells whether it is in menu categories or menu items
   var menu = true;
-
   //initially show guest 1
   $scope.items = DataStore.get().data[0].order;
   //set table number
@@ -30,16 +29,22 @@ function ($scope, $location, DataStore, Categories, Menus) {
   };
   //deal with click events on the menu
   $scope.menuItems = function(item) {
+    //only run if looking at categories, not specific menu items
     if (menu) {
+      menu = false;
+      //create query for specific categories
       var postData = {
-        categories: {
-          categories: item
+        query: {
+          categories: item.currentTarget.getAttribute('data-id')
         }
       };
+      //send request for menu items matching category
       Menus.get(postData)
       .then(function(res){
         //set choices in scope
         $scope.choices = res.data;
+        //force update to occur in view
+        $scope.$apply();
       });
     }
   };
