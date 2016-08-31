3438f0e8e491b448bf142599ed7ba4072e455de8
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-08-31 16:59:55 -0400

    Deleted old implementation of project. Finished numberclick, adddecimal, and calculate functions in public/js/storecheckout.js. Added checkstatus, paid, and change brackets in public/templates/checkout.html.

--- public/js/storecheckout.js
+++ public/js/storecheckout.js
@@ -13,12 +13,13 @@ storeCheckout.controller('Checkout', ['$scope', '$location', '$route', 'DataStor
     $scope.items = tableChecks.order;
     $scope.tip = storeInfo.recommendedTip + '%';
     $scope.tax = storeInfo.stateTax + '%';
+    $scope.checkStatus = 'unpaid';
     //set table number
     $scope.tableNumber = tableChecks.tableId;
     //total up all the ticket items
     $scope.total = tableChecks.order.reduce((prev, current) => {
-      return prev.cost + current.cost;
-    });
+        return {cost: prev.cost + current.cost};
+    }).cost;
     $scope.recommendedTotal = ($scope.total + ($scope.total * (storeInfo.recommendedTip / 100))).toFixed(2);
     //function that returns to showing the categories
     $scope.categorySelect = function() {
@@ -28,13 +29,42 @@ storeCheckout.controller('Checkout', ['$scope', '$location', '$route', 'DataStor
     };
     //function to add number to cash out amount
     $scope.numberClick = function(number) {
-
+      if ($scope.cashAmount) {
+        $scope.cashAmount = (parseFloat('0' + $scope.cashAmount + number));
+      } else {
+        $scope.cashAmount = (parseFloat('0' + number));
+      }
+    };
+    //function to add a decimal to the back of the cash amount
+    $scope.addDecimal = function() {
+      //turn cashamount into a string
+      var temp = $scope.cashAmount.toString();
+      //ensure that there is only one . in the string
+      if ( temp && !temp.includes('.')) {
+        $scope.cashAmount = $scope.cashAmount + '.';
+      }
     };
     //function to enable clicking on number buttons to enter cash amount
     $scope.cash = function() {
       $scope.cashStatus = false;
     };
+    //function to show amount paid and remove check
     $scope.calculate = function() {
-      
+      //if the amount paid is equal or greater than the amount owed
+      if ($scope.cashAmount >= $scope.total) {
+        //display paid and change on the screen
+        $scope.paid = $scope.cashAmount;
+        $scope.change = ($scope.cashAmount - $scope.total).toFixed(2);
+        //disable buttons
+        $scope.cashStatus = true;
+        Tables.remove(tableChecks._id)
+        .then((res) => {
+          //change status on check to paid
+          $scope.checkStatus = 'paid';
+          //force update to occur in view
+          $scope.$apply();
+        });
+      }
+
     };
   }]);
