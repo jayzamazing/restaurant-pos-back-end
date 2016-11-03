3a3e15cc0ec88aee7ea40ff28f67828d6fe5aa73
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-11-02 21:30:16 -0400

    Added html file and favicon file names and installed copy-webpack-plugin, extract-loader and file-loader in package.json. Changed image urls for all images in public/templates/tables.html. Added configuration to get images and favicon file automatically from html files and placed inside build folder in webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var packageData = require('./package.json');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var minify = process.argv.indexOf('--minify') !== -1;
 var filename = [packageData.name, packageData.version, 'js'];
 var plugins = [];
@@ -24,7 +25,7 @@ module.exports = [
           test: /\.js$/,
           exclude: /(node_modules)/,
           loader: 'babel'
-        }
+        },
       ]
     },
     devtool: 'source-map',
@@ -32,5 +33,37 @@ module.exports = [
       new webpack.optimize.CommonsChunkPlugin(
         'vendor', 'bundle.js')
     ]
+  },
+  {
+    entry: {
+      app: packageData.html
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.')
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(html)$/,
+          loader: 'file-loader?name=[name].[ext]!extract-loader!html-loader'
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'file-loader?name=[name].[ext]'
+        }
+      ]
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(packageData.version)
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: packageData.ico, to: './'
+        }
+      ])
+    ]
   }
-];
+]
