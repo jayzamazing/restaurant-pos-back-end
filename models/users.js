eff248def40ea87157614a459bf40d6262ec950e
jayzamazing <adrianjaylopez@gmail.com>
2017-12-09 13:23:25 -0500

    Added users model.

---
+++ models/users.js
@@ -0,0 +1,41 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//schema representing a user
+const userSchema = mongoose.Schema({
+  email: {type: String, required: true, unique: true},
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  roles: [{ type: String, required: true }],
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+userSchema.methods.apiRepr = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    fullName: this.fullName,
+    roles: this.roles,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt
+  };
+};
+userSchema.methods.validateRole = function(role) {
+  return this.roles.includes(role)
+}
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt
+    .compare(password, this.password)
+    .then(isValid => isValid);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt
+    .hash(password, 10);
+};
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = {User};
